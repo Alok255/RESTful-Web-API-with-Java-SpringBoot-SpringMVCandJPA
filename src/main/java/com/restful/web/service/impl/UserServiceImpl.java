c99a89e4e8920dfb60704dd38ec6d4adf2075ed7
alok255 <alokrajsingh255@gmail.com>
2021-02-12 19:02:27 -0500

    added getUserById API Endpoint

--- src/main/java/com/restful/web/service/impl/UserServiceImpl.java
+++ src/main/java/com/restful/web/service/impl/UserServiceImpl.java
@@ -66,4 +66,13 @@ public class UserServiceImpl implements UserService {
         if(userEntity == null) throw new UsernameNotFoundException(email);
         return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), new ArrayList<>());
     }
+
+    @Override
+    public UserDto getUserByUserId(String userId){
+        UserDto returnValue = new UserDto();
+        UserEntity userEntity = userRepository.findByUserId(userId);
+        if(userEntity == null) throw new UsernameNotFoundException(userId);
+        BeanUtils.copyProperties(userEntity, returnValue);
+        return returnValue;
+    }
 }
